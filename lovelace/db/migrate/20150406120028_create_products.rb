70287ea4a72ff7036c25e0133515f44c883f6965
ecmeirelles <ecmeirelles@hotmail.com>
2015-04-06 09:12:32 -0300

    Create a Product model

---
+++ lovelace/db/migrate/20150406120028_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.integer :code
+      t.string :name
+      t.string :image
+      t.float :price
+      t.integer :stock
+      t.text :description
+      t.text :details
+
+      t.timestamps null: false
+    end
+  end
+end
