154ede3ff4e901a09e544845950e22c9d48f22cc
Attany <nathalyaraujoo@gmail.com>
2015-04-09 11:09:16 -0300

    Creating the create method and a test view

--- lovelace/app/controllers/subcategories_controller.rb
+++ lovelace/app/controllers/subcategories_controller.rb
@@ -3,4 +3,16 @@ class SubcategoriesController < ApplicationController
 	def new
 	end
 
+	def create
+		@subcategory = Subcategory.new(subcategory_params)
+ 
+  		@subcategory.save
+  		redirect_to @subcategory
+	end
+
+	private
+  		def subcategory_params
+    		params.require(:subcategory).permit(:name, :description)
+ 		 end
+
 end
