4ca90dceedb3de29973f6e048aff2dc9f68cd424
Attany <nathalyaraujoo@gmail.com>
2015-04-10 17:07:31 -0300

    Creating validations in subcategory model

--- lovelace/app/models/subcategory.rb
+++ lovelace/app/models/subcategory.rb
@@ -1,2 +1,9 @@
 class Subcategory < ActiveRecord::Base
+
+	validates :name, presence: true, 
+	                 length: {maximum:  100}
+
+	validates :description, presence: true, 
+	                        length: {maximum:  400}
+
 end
