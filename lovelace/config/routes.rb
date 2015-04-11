c11d89c36573f27f3c3514f1b95bc54addab463f
vannessasoares <vannessa.andrade@gmail.com>
2015-04-11 10:56:51 -0300

    Add CRUD methods in Categories Controller

--- lovelace/config/routes.rb
+++ lovelace/config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   get 'welcome/homepage'
   resources :products
+  resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :products
 
   # You can have the root of your site routed with "root"
   root 'welcome#homepage'
