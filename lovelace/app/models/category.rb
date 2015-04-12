613860bf51d59683de64e2cc13511ed76129651e
vannessasoares <vannessa.andrade@gmail.com>
2015-04-12 15:49:30 -0300

    Add validates in Category Model

--- lovelace/app/models/category.rb
+++ lovelace/app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ActiveRecord::Base
 	validates :name, presence: true, 
 					 uniqueness: true,
 	                 length: {maximum:  50}
+	                 
+	validates :description,  
+	                        length: {maximum:  150}
 end
