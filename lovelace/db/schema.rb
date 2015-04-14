4897be0543c2cb9869370506269775396e895d02
ecmeirelles <ecmeirelles@hotmail.com>
2015-04-13 23:50:25 -0300

    Solving db problems

--- lovelace/db/schema.rb
+++ lovelace/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406155027) do
+ActiveRecord::Schema.define(version: 20150410111954) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.integer  "code",               limit: 4
@@ -26,7 +33,13 @@ ActiveRecord::Schema.define(version: 20150406155027) do
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
+  end
 
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
 end
