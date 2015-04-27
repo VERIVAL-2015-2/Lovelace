c61ac8ae73ec4a9fcaf6d28f8e5f53eadf5d2351
ecmeirelles <ecmeirelles@hotmail.com>
2015-04-27 16:12:03 -0300

    Adding products tests: unit and controller

--- lovelace/test/functional/products_controller_test.rb
+++ lovelace/test/functional/products_controller_test.rb
@@ -1,22 +1,59 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase 
-	fixtures :products
+  fixtures :products
 
-  test "should get index" do
+  # called before every single test
+  def setup
+    @products = products(:kit_lapis)
+  end
+
+  # called after every single test
+  def teardown
+    # when controller is using cache it may be a good idea to reset it afterwards
+    Rails.cache.clear
+  end
+
+  #simulates a request on the action called index
+  def test_should_get_index   
     get :index
+    assert_response :success #making sure the request was successful
+    assert_not_nil (:products)  # ensuring that it assigns a valid products instance variable.
+  end
+
+  def test_should_create_a_product
+    assert_difference('Product.count') do
+      post :create, :products => { :id => @products.id,
+                                   :code => @products.code, 
+                                   :name => @products.name,
+                                   :price => @products.price,
+                                   :stock => @products.stock,
+                                   :description => @products.description,
+                                   :details => @products.details
+                                 }
+    end
+
+    assert_redirected_to products_path(assigns(:products))
+    assert_equal 'Produto criado com sucesso!', flash[:notice]
+    assert_not_nil Product.find_by(code: 201234)
+
+  end
+
+  def test_should_not_save_product_without_required_fields
+    product = Product.new
+    assert_not product.save, "Produto salvo sem código."
+  end
+
+  def test_should_show_products
+    # Reuse the @product instance variable from setup
+    get :show, id: @products.id
     assert_response :success
   end
 
-  test  "should show products" do
-  	get(:show, :id => 1)
-	assert_response :success
-  end
 
-  test "should edit product" do
-	 get(:show, :id => 1)
-	 assert_response :success
+  def test_should_edit_product
+    get(:edit, :id => 1)
+    assert_response :success
   end
-
 
 end
