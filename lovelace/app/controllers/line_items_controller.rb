c4af2e3c78f0c397c982a5328d3274ebfffc5012
ecmeirelles <ecmeirelles@hotmail.com>
2015-05-10 20:11:05 -0300

    Creating a basket

--- lovelace/app/controllers/line_items_controller.rb
+++ lovelace/app/controllers/line_items_controller.rb
@@ -24,15 +24,15 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
-
+    @basket = current_basket
+    product = Product.find(params[:product_id])
+    @line_item = @basket.line_items.build(:product => product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to(@line_item.basket,
+        :notice => 'Linha de item criada.') } format.xml {...}
       else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        ...
       end
     end
   end
