084445682b1c3e69791d73929a102eadb349c129
ecmeirelles <ecmeirelles@hotmail.com>
2015-05-10 19:57:58 -0300

    Generate line_item scaffold

--- lovelace/config/routes.rb
+++ lovelace/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'products/new'
 
   get 'subcategories/new'
@@ -16,6 +17,7 @@ Rails.application.routes.draw do
   resources :subcategories
   resources :baskets
   resources :admin 
+  resources :line_items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- lovelace/db/schema.rb
+++ lovelace/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510222841) do
+ActiveRecord::Schema.define(version: 20150510224858) do
 
   create_table "baskets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150510222841) do
     t.string   "description", limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "product_id", limit: 4
+    t.integer  "basket_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "products", force: :cascade do |t|
