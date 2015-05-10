17d32961f9fa2e101408b737c7cbeee57418778c
vannessasoares <vannessa.andrade@gmail.com>
2015-05-09 21:32:51 -0300

    Add atributte admin in user table

--- lovelace/db/schema.rb
+++ lovelace/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509235840) do
+ActiveRecord::Schema.define(version: 20150510000929) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150509235840) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  limit: 1
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
