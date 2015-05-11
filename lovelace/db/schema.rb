668896009bd44061a16be25656021324334ef594
Tainara Santos Reis <tainara.s.r@hotmail.com>
2015-05-11 00:10:30 -0300

    Changing schema

--- lovelace/db/schema.rb
+++ lovelace/db/schema.rb
@@ -36,11 +36,10 @@ ActiveRecord::Schema.define(version: 20150508010740) do
   end
 
   create_table "subcategories", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "description",     limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "add_foreign_key", limit: 255
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "users", force: :cascade do |t|
