20ad029108cac8d952490f898c5b8013a5ebd15d
ecmeirelles <ecmeirelles@hotmail.com>
2015-06-17 23:34:35 -0300

    Adicionando migrations

--- lovelace/db/schema.rb
+++ lovelace/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510224858) do
+ActiveRecord::Schema.define(version: 20150618002737) do
 
   create_table "baskets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20150510224858) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id", limit: 4
     t.integer  "basket_id",  limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "quantity",   limit: 4, default: 1
   end
 
   create_table "products", force: :cascade do |t|
