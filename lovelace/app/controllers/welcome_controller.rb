6be20b00be739c978fd02c74140c55c8fa0a9f00
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2015-12-01 23:26:08 -0200

    Adding more unit tests to WelcomeController and Model Product

--- lovelace/app/controllers/welcome_controller.rb
+++ lovelace/app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!, :except => [:hompeage, :products_catalog, :faq, :services, :contact]
   
   def homepage
   	@products = Product.all
